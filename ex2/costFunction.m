139bf8f12cf1ba1ea9f1cccdd488a7f9d34929c4
Matthew Lowery <mattlowery@Matthews-MBP.lan>
2021-05-13 17:36:24 -0400

    finished ex2 ~ wk3

--- ex2/costFunction.m
+++ ex2/costFunction.m
@@ -19,9 +19,12 @@ grad = zeros(size(theta));
 %
 % Note: grad should have the same dimensions as theta
 %
+h = sigmoid(X*theta);
 
+% vectorized implementation
+J = (-y'*log(h) - (1-y)'*log(1-h))/m;
 
-
+grad = (1/m) * X' * (h-y);
 
 
 
